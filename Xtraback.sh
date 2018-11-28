762e9041360a55dd81db48f6683681639063632c
Scong <sc4242@163.com>
2018-11-28 17:07:03 +0800

    Xtranback Full Backup & Incremental Backup

--- Xtraback.sh
+++ Xtraback.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # By Scong
-# Date: 2018-11-27  v1.4
+# Date: 2018-11-27  v1.5
 
 ############################## Xtranback Full Backup & Incremental Backup ##############################
 
 # 数据库
 User=Scong
 Password=123456
+
+# 指定MySQL的配置文件,这个操作主要是为了防止不同机器，MySQL的配置文件路径不一样
+Configure_Dir=/etc/my.cnf
 
 # 时间戳
 Date=`date +'%Y%m%d'`
@@ -23,13 +26,21 @@ Incre_Backup=${Base_Dir}increment-${Date}
 # 上一次备份文件的路径
 Last_Backup=$(ls ${Base_Dir} | tail -1 | cut -d\' -f2)
 
+# 检测Xtrabackup是否安装
+if [ -f /usr/bin/innobackupex ];then
+    echo "is ok"
+else
+    echo "is not exist"
+fi
+
 # 判断是否有进行过全备，如果无则进行全备，如果有则进行增备
 if [ ! -d "${All_Backup}" ];then
+
 # 判断是否为周日，如果是，重新进行一次全备，如果不是，则进行增量备份
 #if [ $(date +%w) -eq 0 ]; then
     echo -e "\n Full backup starts... ... Please wait a moment\n"
     sleep 2
-    innobackupex --user ${User} --password ${Password} ${All_Backup} --no-timestamp
+    innobackupex --defaults-file=${Configure_Dir} --user ${User} --password ${Password} ${All_Backup} --no-timestamp >/dev/null 2>&1
     if [ $? = 0 ];then
       echo -e "\nComplete Successfully!"
     else
@@ -38,7 +49,7 @@ if [ ! -d "${All_Backup}" ];then
 else
     echo -e "\nIncremental backup started... ...Please wait a moment\n"
     sleep 2
-    innobackupex --user ${User} --password ${Password} --incremental ${Incre_Backup} --incremental-basedir=${Base_Dir}${Last_Backup} --no-timestamp
+    innobackupex --defaults-file=${Configure_Dir} --user ${User} --password ${Password} --incremental ${Incre_Backup} --incremental-basedir=${Base_Dir}${Last_Backup} --no-timestamp >/dev/null 2>&1
     if [ $? = 0 ];then
       echo -e "\nIncremental Successfully!!!"
     else
@@ -54,7 +65,7 @@ if [ $(date +%w) -eq 0 ]; then
    rm -rf /var/lib/mysql/*
    echo -e  "\nPrepare for your first data recovery......Please wait a moment\n"
    sleep 2
-   innobackupex --user ${User} --password ${Password} --apply-log --redo-only  ${All_Backup}
+   innobackupex --user ${User} --password ${Password} --apply-log --redo-only  ${All_Backup} >/dev/null 2>&1 
 
 # 2、依次进行增量恢复，注意这里的6，要根据实际情况进行修改
    Dirs=`ls ${Base_Dir} | sort | tail -6`
@@ -62,7 +73,7 @@ if [ $(date +%w) -eq 0 ]; then
    echo -e "\nLog merge in progress... ...Please wait a moment\n"
    for dirs in ${Dirs_arr[@]}
    do
-      innobackupex --user ${User} --password ${Password} --apply-log --redo-only ${All_Backup} --incremental-dir=${Base_Dir}${dirs}
+      innobackupex --user ${User} --password ${Password} --apply-log --redo-only ${All_Backup} --incremental-dir=${Base_Dir}${dirs}	>/dev/null 2>&1
       if [ $? -eq 0 ];then
          echo "${dirs},Restore Successfully！"
 		 sleep 2
@@ -75,7 +86,7 @@ if [ $(date +%w) -eq 0 ]; then
 #将恢复好的全备数据导入使用（非必须操作）
 echo -e "\nDatabase copy in progress......Do not operate..."
 sleep 2
-innobackupex --user ${User} --password ${Password} --copy-back ${All_Backup}
+innobackupex --user ${User} --password ${Password} --copy-back ${All_Backup} >/dev/null 2>&1
 
 # 将恢复好的全备数据进行打包留底
 tar -cPf ${All_Backup}.tar.gz ${All_Backup}
@@ -85,6 +96,14 @@ chown -R mysql.mysql /var/lib/mysql
 
 # 重启mysqld服务
 systemctl restart mysqld
+
+# 检测MySQL是否重启成功
+netstat -antulp | grep :3306 >/dev/null 2>&1
+if [ $? = 0 ];then
+    echo "Database started successfully!" 
+else
+	echo "Database started Failed!"
+fi
 
 # 查找一星期前的文件并将其删除
 #find ./  -mindepth 1 -maxdepth 1 -type d -name '*' -ctime +7 -exec rm -rf {} \;
